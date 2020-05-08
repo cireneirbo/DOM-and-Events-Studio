64def1da4c0f00b754fee72308522e38516addc1
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-05-08 18:24:27 -0400

    formatting and window.confirm

--- scripts.js
+++ scripts.js
@@ -1,2 +1,10 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+document.addEventListener("DOMContentLoaded", function(){
+    
+});
+
+const takeOffButton = document.getElementById("takeoff");
+takeOffButton.addEventListener("click", function() {
+    window.confirm("Confirm that the shuttle is ready for takeoff.")
+});
